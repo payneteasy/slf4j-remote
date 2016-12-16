c81650d4e8950338ecc30a4d8f18a416fec5cf39
Evgeniy Sinev <es@payneteasy.com>
2016-12-16 12:23:19 +0300

    Do not use StandardCharsets.UTF_8, change version to 1.0-16

--- android/src/main/java/io/pne/logger/uploader/http/MessageFormatter.java
+++ android/src/main/java/io/pne/logger/uploader/http/MessageFormatter.java
@@ -2,14 +2,16 @@ package io.pne.logger.uploader.http;
 
 import org.slf4j.event.Level;
 
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.util.Date;
 
 public class MessageFormatter {
 
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+
     public byte[] createMessage(Level aLevel, String aTag, String aMessage) {
         String text = new Date() + " " + aLevel + " " + aTag + " " + aMessage + "\n";
-        return text.getBytes(StandardCharsets.UTF_8);
+        return text.getBytes(UTF_8);
     }
 
 }
